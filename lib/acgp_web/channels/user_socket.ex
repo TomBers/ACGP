597ca3aa783c9f8addf4afc5a554dda2257909f2
Tom Berman <tjcberman@gmail.com>
2020-04-28 21:55:39 +0100

    try example from https://hashrocket.com/blog/posts/revisiting-a-video-chat-application-with-phoenix-and-webrtc

--- lib/acgp_web/channels/user_socket.ex
+++ lib/acgp_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule AcgpWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", AcgpWeb.RoomChannel
+   channel "video:*", AcgpWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
