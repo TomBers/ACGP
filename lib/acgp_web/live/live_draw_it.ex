079276258d9b45b83fab25f6a6b91fbcd5c7db3f
Tom Berman <tjcberman@gmail.com>
2020-05-02 11:34:03 +0100

    Add canvas

---
+++ lib/acgp_web/live/live_draw_it.ex
@@ -0,0 +1,39 @@
+defmodule AcgpWeb.LiveDrawIt do
+  use Phoenix.LiveView
+
+  alias AcgpWeb.Presence
+
+  defp topic(id), do: "room:#{id}"
+
+  def mount(_something, %{"id" => room}, socket) do
+    prefix = GameUtils.get_name()
+    uid = GameUtils.get_id()
+
+    name = "#{prefix}_#{uid}"
+
+    Presence.track_presence(
+      self(),
+      topic(room),
+      name,
+      %{
+        name: name,
+        score: 0,
+        is_card_czar: false,
+      }
+    )
+
+    AcgpWeb.Endpoint.subscribe(topic(room))
+
+
+    {:ok,
+      assign(socket,
+        room: room,
+        my_name: name,
+        users: Presence.list_presences(topic(room)) )}
+  end
+
+  def handle_info(%{event: "presence_diff", payload: payload}, socket) do
+    {:noreply, socket |> assign(users: Presence.list_presences(topic(socket.assigns.room)))}
+  end
+
+end
