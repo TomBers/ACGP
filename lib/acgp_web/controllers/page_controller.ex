af210c335db04f9b7aa67b1c0012e05d58c5092d
Tom Berman <tjcberman@gmail.com>
2020-05-31 10:31:16 +0100

    Missing end

--- lib/acgp_web/controllers/page_controller.ex
+++ lib/acgp_web/controllers/page_controller.ex
@@ -21,10 +21,10 @@ defmodule AcgpWeb.PageController do
 
   def answerwrong(conn, %{"id" => id}) do
     live_render(conn, AcgpWeb.LiveAnswerWrong, session: %{"id" => id})
+  end
 
   def abundance(conn, %{"id" => id}) do
     live_render(conn, AcgpWeb.LiveAbundance, session: %{"id" => id})
-
   end
 
 end
