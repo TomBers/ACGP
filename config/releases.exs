1d823c2e4bd24210424d4334cba43f36a8bd39dd
Tom Berman <tjcberman@gmail.com>
2021-01-04 17:46:05 +0000

    Remove ref to Scrupulus

--- config/releases.exs
+++ config/releases.exs
@@ -2,10 +2,10 @@ import Config
 
 db_url = System.get_env("DATABASE_URL")
 
-config :scrupulous, AcgpWeb.Endpoint,
+config :acgp, AcgpWeb.Endpoint,
   secret_key_base: "LkrIIPvPC9XgMqkhgH2RsOn+ByJ+Th/dVRKSxxjE39A+oeEPIal8Gr3DXOffUKjR"
 
 # Configure your database
-config :scrupulous, Acgp.Repo,
+config :acgp, Acgp.Repo,
   url: db_url,
   pool_size: 15
